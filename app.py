b4ef657e32990be975d5eb6fcea0845e033a9513
czkyyz <752371688@qq.com>
2018-05-26 17:17:16 +0800

    challenge2

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 import json
 import os
 from flask import Flask, render_template, abort
+from flask_sqlalchemy import SQLAlchemy
+
 
 result = {}
 
-d = os.path.join(os.path.abspath(os.path.dirname(__name__)), '..', 'files')
+d = os.path.join(os.path.abspath(os.path.dirname(__name__)), '..', 'files') 
+
 for i in os.listdir(d):
     file_path = os.path.join(d, i)
     with open(file_path) as f:
         result[i[:-5]] = json.load(f)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/xiaoshuo'
+db = SQLAlchemy(app)
+
+class File(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80))
+    created_time = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer,db.ForeignKey(...))
+    #category_id = db.Column(db.ForeignKey(category_id))
+    content = db.Column(db.Text)
+
+class Category(db.Model):
+    id = db.Column(db.Integer)
+    name = db.Column(db.String(80))
+
 
 @app.errorhandler(404)
 def not_found(error):
