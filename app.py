e0ee5d1672269ba8f04344b64609d5298b216e51
czkyyz <752371688@qq.com>
2018-05-27 12:56:49 +0800

    kazhu

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
-import json
-import os
+#import os
+#import json
+from datetime import date,datetime
 from flask import Flask, render_template, abort
 from flask_sqlalchemy import SQLAlchemy
-
-
-result = {}
-
-d = os.path.join(os.path.abspath(os.path.dirname(__name__)), '..', 'files') 
-
-for i in os.listdir(d):
-    file_path = os.path.join(d, i)
-    with open(file_path) as f:
-        result[i[:-5]] = json.load(f)
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/xiaoshuo'
-db = SQLAlchemy(app)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from yourapplication import db
+#app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/test'
+#db = SQLAlchemy(app)
 
 class File(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     title = db.Column(db.String(80))
     created_time = db.Column(db.DateTime)
-    category_id = db.Column(db.Integer,db.ForeignKey(...))
-    #category_id = db.Column(db.ForeignKey(category_id))
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    category = db.relationship('Category', backref='file')
     content = db.Column(db.Text)
+    def __init__(self,title,created_time,category,content):
+        self.title = title
+        self.created_time = created_time
+        self.category = category
+        self.content = content
+    def __repr__():
+        return '<File %r>'%self.title
 
 class Category(db.Model):
-    id = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
-
-
+    def __init__(self,name):
+        self.name = name
+    def __repr__():
+        return '<Category %r>'%self.name
+'''
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
@@ -48,7 +51,21 @@ def file(filename):
             return (_title['content'])
     else:
         not_found(error)
-
+'''
 if __name__ == '__main__':
-    app.run()
-
+#    app.run()
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost/test'
+    db = SQLAlchemy(app)
+    db.create_all()
+    java = Category('Java')
+    python = Category('Python')
+    file1 = File("Hello Java",datetime.utcnow(),java,'File Content-Java is cool!')
+    file2 = File('Hello Python',datetime.utcnow(),python,'File Content - Python is cool!')
+    db.session.add(java)
+    db.session.add(python)
+    db.session.add(file1)
+    db.session.add(file2)
+    db.session.commit()
+    engine = create_engine('mysql://root@localhost/test')
+    engine.execute('select * from file').fetchall()
